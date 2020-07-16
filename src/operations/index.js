f4da30f21877363f791d1d085374382db08db19c
Gustavo Gondim <gustavospgondim@gmail.com>
2020-07-16 17:25:23 -0300

    .

--- src/operations/index.js
+++ src/operations/index.js
@@ -1,7 +1,7 @@
 module.exports = {
-  createSingle: require('./create-single'),
-  retrieveSingle: require('./retrieve-single'),
-  retrieveMultiple: require('./retrieve-multiple'),
-  updateSingle: require('./update-single'),
-  deleteSingle: require('./delete-single'),
+  createSingle: require('./create-single.js'),
+  retrieveSingle: require('./retrieve-single.js'),
+  retrieveMultiple: require('./retrieve-multiple.js'),
+  updateSingle: require('./update-single.js'),
+  deleteSingle: require('./delete-single.js'),
 };
